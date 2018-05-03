c19843f78a38cbd87b650b6756bfe83a55a3ed7f
foreverlpficloud.com <foreverlpficloud.com@forevercomdembp.lan>
2018-05-04 00:09:33 +0800

    add Dynamic Proxy Pattern

--- src/designpattern/proxy/dynamicproxy/Client.java
+++ src/designpattern/proxy/dynamicproxy/Client.java
@@ -1,13 +1,15 @@
 package designpattern.proxy.dynamicproxy;
 
-import designpattern.proxy.staticproxy.Proxy;
-import designpattern.proxy.staticproxy.RealSubject;
-import designpattern.proxy.staticproxy.Subject;
+import java.lang.reflect.InvocationHandler;
 
 public class Client {
 	public static void main(String[] args) {
+		//定义主题
 		Subject real = new RealSubject();
-		Subject proxy = new Proxy(real);
-		proxy.request();
+		//定义Handler
+		//InvocationHandler handler = new MyInvocationHandler(real);
+		//Subject proxy = DynamicProxy.newProxyInstance(real.getClass().getClassLoader(), real.getClass().getInterfaces(), handler);
+		Subject proxy = SubjectDynamicProxy.newProxyInstance(real);
+		proxy.doSomething("I am a Real Man,Eat a lot.");
 	}
 }
