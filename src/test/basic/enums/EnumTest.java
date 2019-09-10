4d777b3330a0382d4064883eeb1e8189b8244e9e
RunAtWorld <hudalpf@163.com>
2019-09-10 14:22:40 +0800

    Create EnumTest.java

---
+++ src/test/basic/enums/EnumTest.java
@@ -0,0 +1,9 @@
+package test.basic.enums;
+
+public class EnumTest {
+    public static void main(String[] args) {
+        System.out.println(ErrorCategory.BusinessChange.id + "==>" +ErrorCategory.BusinessChange.content);
+        System.out.println(ErrorCategory.BusinessChange.id + "==>" +ErrorCategory.BusinessChange.content);
+        System.out.println(ErrorCategory.fromId(0).content);
+    }
+}
