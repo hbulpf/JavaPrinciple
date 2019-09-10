8fad09b2d45b26718cb85001cd87e6ee0aeea587
RunAtWorld <hudalpf@163.com>
2019-09-10 14:29:36 +0800

    Update DateUtils.java

--- src/dev/utils/DateUtils.java
+++ src/dev/utils/DateUtils.java
@@ -16,18 +16,4 @@ public class DateUtil {
         return new Timestamp(System.currentTimeMillis());
     }
 
-
-    /**
-     * 获取一天的开始 UTC时间
-     * @return
-     */
-    public static String  getBeginTimeOfDay() {
-        Calendar cal = Calendar.getInstance();
-        //服务器是UTC时间,一天开始时间东八区CST 24:00 - 8为服务器对应的时间，即UTC 16:00
-        if (cal.get(Calendar.HOUR_OF_DAY) < 16) {
-            cal.add(Calendar.DAY_OF_YEAR, -1);
-        }
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        return sdf.format(cal.getTime()) + "T16:00:00Z";
-    }
 }
