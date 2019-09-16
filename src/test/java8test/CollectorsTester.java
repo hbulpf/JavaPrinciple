f59e4af6c6578385b188a686da679a2b0e447657
RunAtWorld <hudalpf@163.com>
2019-09-16 15:24:58 +0800

    java8 提取出list对象中的一个属性

--- src/test/java8test/CollectorsTester.java
+++ src/test/java8test/CollectorsTester.java
@@ -17,17 +17,38 @@ public class CollectorsTester {
         list.add(new Person(1, "haha"));
         list.add(new Person(2, "rere"));
         list.add(new Person(3, "fefe"));
+        list.add(new Person(4, "fefe"));
+        list.add(new Person(5, "fefe"));
 
-        Map<Integer, Person> mapp = list.stream().collect(Collectors.toMap(Person::getId, Function.identity()));
-        System.out.println(mapp);
-        System.out.println(mapp.get(1).getName());
-        Map<Integer, String> map = list.stream().collect(Collectors.toMap(Person::getId, Person::getName));
-        System.out.println(map);
+        //1.1 将List转成 <id,Person> 的map
+        Map<Integer, Person> map1 = list.stream().collect(Collectors.toMap(Person::getId, Function.identity()));
+        System.out.println(map1);
+        System.out.println(map1.get(1).getName());
 
-        //Collectors.toMap(keyMapper,valueMapper,mergeFunction)
-        Map<String, Person> map2 = list.stream().collect(Collectors.toMap(Person::getName,b -> b, (k1, k2) -> k1));
+        //1.2 将List转成 <id,Name> 的map
+        Map<Integer, String> map2 = list.stream().collect(Collectors.toMap(Person::getId, Person::getName));
         System.out.println(map2);
 
+        //1.3 将List转成 <Name,Person> 的map
+        Map<String, Person> map3 = list.stream().collect(Collectors.toMap(Person::getName, b -> b, (k1, k2) -> k1));
+        System.out.println(map3);
 
+        //1.4 将List转成 <Name,Person> 的map:这里和1.3不一样，取Name相同的最后一个Person
+        Map<String, Person> map4 = list.stream().collect(Collectors.toMap(Person::getName, b -> b, (k1, k2) -> k2));
+        System.out.println(map4);
+
+        System.out.println("++++++++++++++++++");
+
+        //2.1 提取出list对象中的一个属性
+        List<Integer> pIds = list.stream()
+                .map(Person::getId)
+                .collect(Collectors.toList());
+        pIds.forEach(s -> System.out.print(s + " "));
+        System.out.println();
+        System.out.println("----------");
+
+        //2.2 提取出list对象中的一个属性并去重
+        List<String> pNames = list.stream().map(Person::getName).distinct().collect(Collectors.toList());
+        pNames.forEach(s -> System.out.print(s + " "));
     }
 }
