e114f2a44276c6b8ef7eca608abec059955e0795
RunAtWorld <hudalpf@163.com>
2019-09-19 10:55:47 +0800

    Update Prime.java

--- src/oj/Prime.java
+++ src/oj/Prime.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Prime {
+    /**
+     * 求小于MAX的所有素数
+     * 使用 筛选法 
+     */    
     public static List<Integer> getPrimes(int MAX){
         Integer[] isPrime = new Integer[MAX];
         for(int i=0;i<MAX;i++){
@@ -37,4 +41,18 @@ public class Prime {
         long end = System.currentTimeMillis();
         System.out.println("time consumed: "+(end-start));
     }
+    
+        /**
+         * 常规方法判断是否素数 ， 效率一般
+         * @param n
+         * @return
+         */
+        public static boolean isPrime (long n){
+            for (long i = 2; i*i <= n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
 }
