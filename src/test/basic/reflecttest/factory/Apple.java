a75fdf4bdd2e367fa08526bd1d96da47dea9403f
RunAtWorld <hudalpf@163.com>
2019-10-14 20:56:16 +0800

    Create Apple.java

---
+++ src/test/basic/reflecttest/factory/Apple.java
@@ -0,0 +1,13 @@
+package dev.lpf.basic.reflecttest.factory;
+
+/**
+ * Apple
+ *
+ *
+ */
+public class Apple implements Fruit{
+    @Override
+    public void eat() {
+        System.out.println("eat Apple");
+    }
+}
