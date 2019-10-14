3aeedbbffe672f476c30d36a842bdb0bd22dd3ef
RunAtWorld <hudalpf@163.com>
2019-10-14 20:54:51 +0800

    Create China.java

---
+++ src/test/basic/reflecttest/entity/China.java
@@ -0,0 +1,13 @@
+package dev.lpf.basic.reflecttest.entity;
+
+/**
+ * 中国
+ *
+ *
+ */
+interface China {
+    public final static String country="中国";
+    public final static int coutrycode=4;
+    public void sayChinese();
+    public void sayHello(String name,int age);
+}
