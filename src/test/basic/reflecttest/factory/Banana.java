9548df6695aef2578ffde26308c4b45da8cac2ee
RunAtWorld <hudalpf@163.com>
2019-10-14 20:56:58 +0800

    Create Banana.java

---
+++ src/test/basic/reflecttest/factory/Banana.java
@@ -0,0 +1,13 @@
+package dev.lpf.basic.reflecttest.factory;
+
+/**
+ * Banana
+ *
+ *
+ */
+public class Banana implements Fruit{
+    @Override
+    public void eat() {
+        System.out.println("eat Banana");
+    }
+}
