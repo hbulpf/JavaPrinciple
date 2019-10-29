bd185ddb6fd73b1ed983a4ec09e2ef93e71fbf88
RunAtWorld <hudalpf@163.com>
2019-10-29 15:59:21 +0800

    Update ClassTest.java

--- src/test/basic/classz/ClassTest.java
+++ src/test/basic/classz/ClassTest.java
@@ -14,4 +14,30 @@ public class ClassTest {
         return String.format("%s.%s  line:%d",this.getClass().getTypeName(),
                 Thread.currentThread().getStackTrace()[1].getMethodName(),Thread.currentThread().getStackTrace()[1].getLineNumber());
     }
+    
+     /**
+     * @param obj  操作的对象
+     * @param attr 操作的属性
+     */
+    public static Object getObjField(Object obj, String attr) throws NoSuchFieldException, IllegalAccessException {
+        Field field = obj.getClass().getDeclaredField(attr);
+        field.setAccessible(true);
+        return field.get(obj);
+    }
+
+    /**
+     * @param obj   操作的对象
+     * @param attr  操作的属性
+     * @param value 设置属性值
+     */
+    public static void setObjField(Object obj, String attr, Object value) throws NoSuchFieldException,
+        IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
+        Field field = obj.getClass().getDeclaredField(attr);
+        field.setAccessible(true);
+        Class typeClass = field.getType();
+        System.out.println(typeClass);
+        Constructor constructor = typeClass.getDeclaredConstructor(String.class);
+        Object valObj = constructor.newInstance(value);
+        field.set(obj, valObj);
+    }
 }
