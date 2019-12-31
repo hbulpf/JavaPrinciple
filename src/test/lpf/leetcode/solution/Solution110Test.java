eb18da8ec220263595d5845c054d636e30220dc8
RunAtWorld <hudalpf@163.com>
2019-12-31 09:05:32 +0800

    Update Solution110Test.java

--- src/test/lpf/leetcode/solution/Solution110Test.java
+++ src/test/lpf/leetcode/solution/Solution110Test.java
@@ -1,5 +1,5 @@
 
-package test.lpf.leetcode.solution;
+package test.leetcode.solution;
 
 import dev.lpf.leetcode.solution.Solution110;
 import dev.lpf.leetcode.solution.TreeNode;
@@ -26,8 +26,8 @@ public class Solution110Test extends BaseCase {
         n1.setRight(n4);
         n2.setLeft(n5);
         n2.setRight(n6);
-        System.out.println(solution.isBalanced(n0));
-        assertEquals("abc", "abc");
+        boolean flag = solution.isBalanced(n0);
+        assertEquals(true, flag);
     }
 
     public void test012() {
@@ -39,7 +39,45 @@ public class Solution110Test extends BaseCase {
         n0.setRight(n2);
         n2.setLeft(n5);
         n2.setRight(n6);
-        System.out.println(solution.isBalanced(n0));
-        assertEquals("abc", "abc");
+        boolean flag = solution.isBalanced(n0);
+        assertEquals(false, flag);
+    }
+
+    public void test013() {
+        Solution110 solution = new Solution110();
+        TreeNode n0 = new TreeNode(1);
+        TreeNode n1 = new TreeNode(2);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(3);
+        TreeNode n7 = new TreeNode(4);
+        TreeNode n8 = new TreeNode(4);
+        n0.setLeft(n1);
+        n0.setRight(n2);
+        n1.setLeft(n3);
+        n1.setRight(n4);
+        n3.setLeft(n7);
+        n3.setRight(n8);
+        boolean flag = solution.isBalanced(n0);
+        assertEquals(false, flag);
+    }
+
+    public void test014() {
+        Solution110 solution = new Solution110();
+        TreeNode n0 = new TreeNode(1);
+        TreeNode n1 = new TreeNode(2);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(3);
+        TreeNode n7 = new TreeNode(4);
+        TreeNode n8 = new TreeNode(4);
+        n0.setLeft(n1);
+        n0.setRight(n2);
+        n1.setLeft(n3);
+        n1.setRight(n4);
+        n3.setLeft(n7);
+        n3.setRight(n8);
+        boolean flag = solution.isBalanced(n0);
+        assertEquals(false, flag);
     }
 }
