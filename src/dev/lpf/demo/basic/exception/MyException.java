f02e4df449f6d6396318ee3be5fed41e324473a4
ABCNN <hudalpf@163.com>
2020-01-04 16:12:31 +0800

    add src/dev/lpf/demo/basic/exception/MyException.java.

---
+++ src/dev/lpf/demo/basic/exception/MyException.java
@@ -0,0 +1,29 @@
+package dev.lpf.demo.basic.exception;
+
+/**
+ * 功能描述
+ *
+ */
+public class MyException extends Exception {
+
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public MyException() {
+        super();
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public MyException(String message) {
+        super(message);
+    }
+}
