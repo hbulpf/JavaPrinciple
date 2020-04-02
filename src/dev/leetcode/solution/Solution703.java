ecb4e91233beb8f85219f7a7588988e436eb7fcf
lipengfei <hudalpf@163.com>
2020-04-03 00:14:00 +0800

    数据流中第K大元素使用 优先级队列

--- src/dev/leetcode/solution/Solution703.java
+++ src/dev/leetcode/solution/Solution703.java
@@ -1,16 +1,45 @@
 package dev.leetcode.solution;
 
+import java.util.PriorityQueue;
+
 /**
  * 数据流中第K大元素
+ * 使用 优先级队列,
+ * 优先级队列默认从小到大排序
+ *
  * @Author: RunAtWorld
  * @Date: 2020/4/2 22:36
  */
 public class Solution703 {
-    public void KthLargest(int k, int[] nums) {
 
+    private PriorityQueue<Integer> queue;
+    private int limit;
+
+    public static void main(String[] args) {
+        new Solution703().KthLargest(5, new int[]{1, 2, 5, 4, 6, 2, 9, 21, 89});
+        System.out.println("=====");
+        new Solution703().KthLargest(5, new int[]{1, 4, 6, 2});
+        System.out.println("====");
+        new Solution703().KthLargest(5, new int[]{1, 2, 6, 2, 9});
+    }
+
+    public void KthLargest(int k, int[] nums) {
+        queue = new PriorityQueue<Integer>(k);
+        limit = k;
+        for (int i = 0; i < nums.length; i++) {
+            add(nums[i]);
+        }
     }
 
     public int add(int val) {
-return 0;
+        if (queue.size() < limit) {
+            queue.add(val);
+        } else {
+            if (val > queue.peek()) {
+                queue.poll();
+                queue.add(val);
+            }
+        }
+        return queue.peek();
     }
 }
---
+++ src/test/lpf/leetcode/solution/Solution703Test.java
@@ -0,0 +1,27 @@
+package test.lpf.leetcode.solution;
+
+import dev.leetcode.solution.Solution703;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @Author: RunAtWorld
+ * @Date: 2020/4/3 0:00
+ */
+public class Solution703Test {
+
+    @Test
+    public void add(){
+        System.out.println("=================");
+        Solution703 solution = new Solution703();
+        solution.KthLargest(5, new int[]{1, 2, 5, 4, 6, 2, 9, 21, 89});
+        assertEquals(solution.add(5),5) ;
+        solution.KthLargest(5, new int[]{1, 4, 6, 2});
+        assertEquals(solution.add(3),1) ;
+        solution.KthLargest(3, new int[]{1, 2, 6, 2, 9});
+        assertEquals(solution.add(7),6) ;
+        System.out.println("test OK~");
+        System.out.println("=================");
+    }
+}
