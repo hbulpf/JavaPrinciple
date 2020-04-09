96a1278cde7fd608bd8f29a37dccc80933a685e9
RunAtWorld <hudalpf@163.com>
2020-04-09 08:46:23 +0000

    Add new file

---
+++ src/dev/demo/entity/Employee.java
@@ -0,0 +1,72 @@
+
+package dev.lpf.demo.entity;
+
+import java.io.Serializable;
+
+/**
+ * 雇员信息
+ */
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 4775629632953317597L;
+
+    /**
+     * ID
+     */
+    public int id;
+
+    /**
+     * 级别
+     */
+    public int level;
+
+    /**
+     * 工资
+     */
+    public int salary;
+
+    /**
+     * 入职年数
+     */
+    public int year;
+
+    public Employee(int id, int level, int salary, int year) {
+        this.id = id;
+        this.level = level;
+        this.salary = salary;
+        this.year = year;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+}
