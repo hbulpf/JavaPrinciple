43f31f72c2f8193a3d35d7dbab0bbc1e81220bc4
RunAtWorld <hudalpf@163.com>
2020-04-14 15:54:50 +0800

    Update ListDemo.java

--- src/main/dev/demo/basic/list/ListDemo.java
+++ src/main/dev/demo/basic/list/ListDemo.java
@@ -1,4 +1,4 @@
-package dev.demo.basic.list;
+package dev.lpf.demo.basic.collection.list;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,48 +6,48 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * @program: JavaPrinciple
- * @description: List 使用测试
+ * List Demo
  *
- * https://blog.csdn.net/PitBXu/article/details/97672145
- * https://blog.csdn.net/qq_31564573/article/details/87906309
- * https://www.jianshu.com/p/84e4820993d3
- * @author: RunAtWorld
- * @create: 2019-12-28 18:23
- **/
-public class ListDemo {
+ */
+class ListDemo {
     public static void main(String[] args) {
-        System.out.println("-------test01---------");
-        test01();
-        System.out.println("-------test02---------");
-        test02();
-        System.out.println("-------test03---------");
-        test03();
+        System.out.println("-------Array2List---------");
+        array2List();
+        System.out.println("-------List2Array---------");
+        listAndString();
+        System.out.println("-------List包装---------");
+        listAndInteger();
     }
 
-    public static void test01() {
+    /**
+     * Array2List
+     */
+    public static void array2List() {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 9, 92};
         System.out.println(Arrays.toString(arr));
         //int[] 转换为 List
         List list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+
+        //打印
         int[] arr2 = new int[]{2, 43, 22, 7};
         for (int i : arr2) {
             if (list.contains(Integer.valueOf(i))) {
                 System.out.printf("%d\t",i);
             }
         }
-
         list.add(23);
-
-        System.out.println();
     }
 
-    public static void test02() {
+    /**
+     * List包装String
+     */
+    public static void listAndString() {
         List<String> roledList = new ArrayList<String>();
         roledList.add("1");
         roledList.add("2");
         roledList.add("3");
 
+        //打印
         Integer[] oneRoles = new Integer[]{1, 3};
         for (Integer i : oneRoles) {
             if (roledList.contains(i.toString())) {
@@ -56,7 +56,10 @@ public class ListDemo {
         }
     }
 
-    public static void test03() {
+    /**
+     * List包装Integer
+     */
+    public static void listAndInteger() {
         int[] arr2 = new int[]{2, 43, 22, 7};
         List list = new ArrayList<Integer>();
         list.add(3);
