6a1d8ee3adecd52256c9324a59f2f67e16df65a6
lipengfei <hudalpf@163.com>
2020-04-15 23:34:51 +0800

    链表节点

---
+++ src/main/dev/leetcode/solution/ListNode.java
@@ -0,0 +1,16 @@
+package dev.leetcode.solution;
+
+/**
+ * 链表节点
+ *
+ * @Author: RunAtWorld
+ * @Date: 2020/4/15 23:26
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
