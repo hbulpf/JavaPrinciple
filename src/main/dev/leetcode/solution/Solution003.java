1135c6475ccd62ee3dcd81179bd432623515aa6d
lipengfei <hudalpf@163.com>
2020-05-07 23:42:54 +0800

    3. 无重复字符的最长子串 双指针法

--- src/main/dev/leetcode/solution/Solution003.java
+++ src/main/dev/leetcode/solution/Solution003.java
@@ -1,11 +1,14 @@
 package dev.leetcode.solution;
 
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 /**
  * 3. 无重复字符的最长子串
  * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
  *
+ * (1) 暴力法 T=O(N^3)
+ * (2) 双指针法 T=O(N)
  * @Author: RunAtWorld
  * @Date: 2020/4/28 0:11
  */
@@ -13,10 +16,14 @@ public class Solution003 {
     public static void main(String[] args) {
         int res = lengthOfLongestSubstring1("abcabcbb");
         System.out.println(res);
+
+        res = lengthOfLongestSubstring2("pwwkew");
+        System.out.println(res);
     }
 
     /**
      * 暴力法
+     *
      * @param s
      * @return
      */
@@ -45,6 +52,32 @@ public class Solution003 {
             set.add(str.charAt(i));
         }
         return true;
+    }
+
+    /**
+     * 双指针法
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring2(String s) {
+        int max = 0;
+        int rk = -1;
+        int len = s.length();
+        HashSet uniqueSet = new HashSet<Character>();
+        for (int i = 0; i < len; i++) {
+            if (i != 0) {
+                //这里要去除掉前一个字符，大意大意！！！
+                uniqueSet.remove(s.charAt(i-1));
+            }
+            while (rk + 1 < len && !uniqueSet.contains(s.charAt(rk + 1))) {
+                uniqueSet.add(s.charAt(rk + 1));
+                rk++;
+            }
+            max = Math.max(max, rk - i + 1);
+            System.out.println(String.valueOf(uniqueSet.stream().collect(Collectors.toList())));
+        }
+        return max;
     }
 
     public int lengthOfLongestSubstring(String s) {
---
+++ src/main/dev/leetcode/solution/Solution011.java
@@ -0,0 +1,59 @@
+package dev.leetcode.solution;
+
+/**
+ * 11. 盛最多水的容器
+ * https://leetcode-cn.com/problems/container-with-most-water/
+ *
+ * (1) 暴力法 T=O(N^2)
+ * (2) 双指针法 T=O(N)
+ * @Author: RunAtWorld
+ * @Date: 2020/5/7 23:22
+ */
+public class Solution011 {
+    public static void main(String[] args) {
+        int ans;
+        ans=maxArea1(new int[]{1,8,6,2,5,4,8,3,7});
+        System.out.println(ans);
+
+        ans=maxArea2(new int[]{1,8,6,2,5,4,8,3,7});
+        System.out.println(ans);
+    }
+
+    /**
+     * 暴力法
+     *
+     * @param height
+     * @return
+     */
+    public static int maxArea1(int[] height) {
+        int area = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                area = Math.max(area, Math.min(height[i], height[j]) * (j - i));
+            }
+        }
+        return area;
+    }
+
+    /**
+     * 双指针法
+     * l 为左指针，从左边开始；r为右指针，从右边开始
+     *
+     * @param height
+     * @return
+     */
+    public static int maxArea2(int[] height) {
+        int l = 0, r = height.length - 1;
+        int area = 0, max = 0;
+        while (l < r) {
+            area = Math.min(height[l], height[r]) * (r - l);
+            max = Math.max(area, max);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return max;
+    }
+}
